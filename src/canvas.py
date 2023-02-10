74cf91ca3786d80dfd8c8489aa3c6dd2d041e42a
Dylan Green <cobaltgoldcsprinks@gmail.com>
2023-02-10 12:23:21 -0700

    Sort Assignments by date for each course

--- src/canvas.py
+++ src/canvas.py
@@ -7,7 +7,7 @@ from userdata import UserData
 
 
 def get_current_courses(canvas: Canvas) -> PaginatedList:
-    return canvas.get_courses(enrollment_state='active')
+    return canvas.get_courses(enrollment_state='active', enrollment_type='student')
 
 def get_assignments_within_period(start: datetime, end: datetime, users: list[UserData]) -> list[Assignment]:
     start_time = start.astimezone(timezone.utc)
@@ -15,7 +15,7 @@ def get_assignments_within_period(start: datetime, end: datetime, users: list[Us
     assignments = []
     for user in map(lambda user: user.make_canvas(), users):
         for course in get_current_courses(user):
-            for assignment in course.get_assignments():
+            for assignment in course.get_assignments(order_by='due_at'):
                 if not hasattr(assignment, "due_at_date"):
                     assignments.append(assignment)
                     continue
